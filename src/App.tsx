6a1700cbedbd396e16cf4611c697062a1ccade82
Gio <tec@seescontabilidade.com.br>
2024-09-13 19:24:55 -0300

    Added navigation menu component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import './App.css'
-import ButtonCustom from './components/ButtonCustom/ButtonCustom'
+import "./App.css";
+import NavMenu from "./components/NavMenu/NavMenu";
 
 function App() {
-
   return (
     <>
-      <ButtonCustom title='botão1' />
-      <ButtonCustom title='botão2' />
-      <ButtonCustom title='botão3' />
-      <ButtonCustom title='botão4'/>
-      <ButtonCustom title='botão5'/>
-
+      <NavMenu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavMenu/NavMenu.tsx
@@ -0,0 +1,31 @@
+export default function NavMenu() {
+  return (
+    <div
+      className="NavMenu"
+      style={{
+        display: "flex",
+        gap: 0,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{}}>logo</div>
+      <div>
+        <div>
+          <a href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+          <a href="#">6</a>
+        </div>
+      </div>
+      <div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+        </div>
+      </div>
+    </div>
+  );
+}
