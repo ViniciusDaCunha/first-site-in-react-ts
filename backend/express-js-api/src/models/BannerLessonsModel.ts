0f82e6566ddeb6eb30c971f36ecd2a3eec72dcfb
Vinicius da Cunha <root@LAPTOP-A6Q855RM.localdomain>
2024-10-11 21:46:39 -0300

    Added backend with express

--- backend/express-js-api/src/models/BannerLessonsModel.ts
+++ backend/express-js-api/src/models/BannerLessonsModel.ts
@@ -9,6 +9,6 @@ export const bannerLessonsMock: BannerLessonsType = {
   mainTitle: "Lessons and insights from 8 years",
   description:
     "Where to grow your business as a photographer: site or social media?",
-  buttonLabel: "Sign up",
+  buttonLabel: "Register",
   buttonUrl: "https://google.com",
 };
